f831e732b5381b0bf82e1d236e5fa1ffc76e8b6f
Niittyvilla2 <milokatu@metropolia.fi>
2024-08-21 20:12:18 +0300

    T4 loppu

--- tehtävät/T2/T2-4.py
+++ tehtävät/T2/T2-4.py
@@ -5,7 +5,16 @@ B_str = input("Anna toinen numero:\n")
 C_str = input("Anna kolmas numero:\n")
 
 # muuta laskuja varen
-
 A = float(A_str)
 B = float(B_str)
 C = float(C_str)
+
+# laskut
+summa = (A+B+C)
+tulo = (A*B*C)
+keskiarvo = ((A+B+C)/3)
+
+# printit
+print("Summa: " + str(summa))
+print("Tulo: " + str(tulo))
+print("Keskiarvo: " + str(keskiarvo))
