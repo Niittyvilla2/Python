00dbe08e59d81709577b84d7f72e73f486b4fccc
Niittyvilla2 <milokatu@metropolia.fi>
2024-08-22 10:59:35 +0300

    T2-6

---
+++ tehtävät/T2/T2-6.py
@@ -0,0 +1,4 @@
+import random
+
+print("Kolmenumeroinen koodisi: " + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)))
+print("Nelinumeroinen koodisi: " + str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6)))
