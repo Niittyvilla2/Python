43ac94b618ac2d07858ad3336619603c239cc064
Niittyvilla <milokatu@metropolia.fi>
2024-12-08 18:19:00 +0200

    t

--- tehtävät/T10/T10-4.py
+++ tehtävät/T10/T10-4.py
@@ -21,6 +21,33 @@ class Auto:
             self.kuljettumatka = self.kuljettumatka + matka
 
 
+class Kilpailu:
+    def __init__(self, pituus, kNimi, autot: list):
+        self.autot = autot
+        self.pituus = pituus
+        self.knimi = kNimi
+
+    def tunti_kuluu(self):
+        #kiihdytys
+        for auto in self.autot:
+            auto.kiihdyta(random.randint(-10, 15))
+
+        #matka
+        for auto in autot:
+            auto.kulje(1)
+
+    def tulosta_tilanne(self):
+        for auto in autot:
+                auto = [auto.rekkari, auto.maxnopus, auto.nopeus, auto.kuljettumatka]
+                print(auto)
+
+    def kilpailu_ohi(self,):
+        pituus = self.pituus
+        for auto in autot:
+            if auto.kuljettumatka > self.pituus:
+                return True
+
+
 #Autot
 autot = []
 
@@ -31,16 +58,4 @@ for i in range(10):
 #Kilpailu
 kilpailu = True
 while kilpailu:
-    #Kiihdytys
-    for auto in autot:
-        auto.kiihdyta(random.randint(-10, 15))
-    #Matka
-    for auto in autot:
-        auto.kulje(1)
-    #Katsotaan voittiko kukaan
-    for auto in autot:
-        if auto.kuljettumatka > 10000:
-            kilpailu = False
-            for auto in autot:
-                auto = [auto.rekkari, auto.maxnopus, auto.nopeus, auto.kuljettumatka]
-                print(auto)
+    pass
--- tehtävät/T11/T11-1.py
+++ tehtävät/T11/T11-1.py
@@ -1,7 +1,7 @@
 class Julkaisu:
-
+    def __init__(self, nimi):
+        self.nimi = nimi
+class Lehti(Julkaisu):
     pass
-class Julkaisu(Lehti):
-    pass
-class Julkaisu(Kirja):
+class Kirja(Julkaisu):
     pass
